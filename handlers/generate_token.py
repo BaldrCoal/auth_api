02215a9113668d53f0b6361c6fcd6a27bdfcc2d7
BaldrCoal <vladnilis98@gmail.com>
2024-06-09 02:43:37 +0300

    fix handlers

--- handlers/generate_token.py
+++ handlers/generate_token.py
@@ -8,12 +8,14 @@ class GenerateToken(BaseHandler):
     async def get(self):
         self.set_header('Content-Type', 'application/json')
         print('generate token')
+        passw = self.get_argument('passw')
         token = str(uuid.uuid4())
         try:
-            os.mkdir(f'./non_used_tokens/{token}')
-            with open(f'./non_used_tokens/{token}/token.txt', 'w') as file:
-                file.write(token)
-            answer = {'result': f'{token}'}
+            if passw == '':
+                os.mkdir(f'./non_used_tokens/{token}')
+                with open(f'./non_used_tokens/{token}/token.txt', 'w') as file:
+                    file.write(token)
+                answer = {'result': f'{token}'}
         except Exception as e:
             print(e)
             answer = {'result': f'{e}'}
